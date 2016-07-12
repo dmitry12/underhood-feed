89a660bdbf1a7db35ad547f8beb6ae3a4bc1efff
Dmitry Astapenko <dmitry@colourbox.com>
2016-07-12 15:45:57 +0200

    Cleaning up!

--- demo.jsx
+++ demo.jsx
@@ -4,7 +4,6 @@ import {render} from 'react-blessed';
 import fetch from 'node-fetch';
 
 import Twitter from 'twitter';
-import moment from 'moment';
 import colors from 'colors';
 
 console.log(`
@@ -14,20 +13,19 @@ console.log(`
         `.green);
 
 var client = new Twitter({
-    // TODO Add env
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
  });
 
-var params = {screen_name: 'jsunderhood'};
+var params = {screen_name: 'eblutr2'};
 
 
 class App extends Component {
   render() {
     return (
-      <box label="underhood.sh"
+      <box label="feeds"
            border={{type: 'line'}}
            style={{border: {fg: 'cyan'}}}>
         <InnerBox />
@@ -40,16 +38,17 @@ class InnerBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        tweets: []
+        tweets: [],
+        count: 50
     };
 
     client.get('statuses/user_timeline', params, (error, tweets, response) => {
       if (!error) {
           console.log('connect')
         let resultTweets = [];
+        console.log(tweets.length);
         tweets
-            .sort((a,b) => new Date(b.created_at) - new Date(a.created_at))
-            .reverse()
+            .sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
             .forEach((t, k) => {
                 resultTweets.push(`${t.created_at.magenta}: ${t.text}\n`);
             })
@@ -59,9 +58,6 @@ class InnerBox extends Component {
     });
   }
 
-  fetchTweets() {
-  }
-
   render() {
     return (
       <box
@@ -69,41 +65,14 @@ class InnerBox extends Component {
            ref="box"
            border={{type: 'line'}}
            style={{border: {fg: 'green'}}}
-            enableMouse
+           enableMouse
+           scrollable
        >
        {this.state.tweets}
       </box>
     );
   }
 }
-
-class ProgressBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {completion: 0};
-
-    const interval = setInterval(() => {
-      if (this.state.completion >= 100)
-        return clearInterval(interval);
-
-      this.setState({completion: this.state.completion + 10});
-    }, 1000);
-  }
-
-  render() {
-    return <progressbar orientation="horizontal"
-                        filled={this.state.completion}
-                        top="80%"
-                        left="center"
-                        height="15%"
-                        width="80%"
-                        label="progress"
-                        border={{type: 'line'}}
-                        style={{border: {fg: 'red'}, bar: {bg: 'red'}}} />
-  }
-}
-
 const screen = blessed.screen({
   autoPadding: true,
   smartCSR: true,
